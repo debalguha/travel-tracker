69d163a24ba0e0a43a17b32a7c2917299ed96c9d
debalguha <debalguha.active@gmail.com>
2023-04-10 00:01:30 +1000

    Tracker meta save

--- src/components/MainTracker.js
+++ src/components/MainTracker.js
@@ -7,6 +7,8 @@ import { InputNumber } from 'primereact/inputnumber';
 import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { RadioButton } from 'primereact/radiobutton';
+import { Checkbox } from 'primereact/checkbox';
+import { Dropdown } from 'primereact/dropdown';
 import { Rating } from 'primereact/rating';
 import { Toast } from 'primereact/toast';
 import { Toolbar } from 'primereact/toolbar';
@@ -17,8 +19,24 @@ import { TrackerService } from '../service/TrackerService';
 import 'primereact/resources/themes/lara-light-indigo/theme.css'
 
 export default function Tracker() {
+    let emptyTrackerCampaign = {
+        campaignName: '',
+        createdBy: 'System',
+        restricted: false,
+        vendor: ''
+    }
 
+    const vendors = [
+        { name: 'Microsoft', code: 'Microsoft' },
+        { name: 'Amazon', code: 'Amazon' },
+        { name: 'Redhat', code: 'Redhat' },
+        { name: 'Heroku', code: 'Heroku' }
+    ]
+
+    const [tracker, setTracker] = useState(emptyTrackerCampaign)
     const [trackerMeta, setTrackerMeta] = useState([]);
+    const [trackerDialog, setTrackerDialog] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
     const toast = useRef(null);
     const dt = useRef(null)
 
@@ -33,7 +51,7 @@ export default function Tracker() {
         return (
             <React.Fragment>
                 <div className="my-2">
-                    <Button label="New" icon="pi pi-plus" severity="sucess" className="mr-2" />
+                    <Button label="New" icon="pi pi-plus" severity="sucess" className="mr-2" onClick={openNew}/>
                     <Button label="Delete" icon="pi pi-trash" severity="danger" />
                 </div>
             </React.Fragment>
@@ -49,6 +67,65 @@ export default function Tracker() {
             </span>
         </div>
     );
+
+    const hideDialog = () => {
+        setTrackerDialog(false)
+    }
+
+    const openNew = () => {
+        setTracker(emptyTrackerCampaign);
+        setSubmitted(false);
+        setTrackerDialog(true);
+    }
+
+    const onInputChange = (e, name) => {
+        const val = (e.target && e.target.value) || '';
+        let _tracker = { ...tracker }
+        _tracker[`${name}`] = val;
+        
+        setTracker(_tracker);
+    };
+
+    const onDropdownChange = (e, name) => {
+        const val = (e.target && e.target.value.code) || '';
+        let _tracker = { ...tracker }
+        _tracker[`${name}`] = val;
+        
+        setTracker(_tracker);
+    }
+
+    const onCheckboxChanged = (e, name) => {
+        
+        let _tracker = { ...tracker }
+        _tracker[`${name}`] = e.checked;
+        
+        setTracker(_tracker);
+    }
+
+    const saveProduct = () => {
+        setSubmitted(true);
+        TrackerService.saveTrackerMeta(tracker, (responseTrackerData) => {
+            let _trackerMeta = [...trackerMeta];
+            //let _tracker = { ...tracker };
+            if(responseTrackerData.id) {
+                _trackerMeta.push(responseTrackerData)
+                setTrackerMeta(_trackerMeta)
+                setTracker(emptyTrackerCampaign)
+                toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Tracker Created', life: 3000 });
+            } else {
+                toast.current.show({ severity: 'danger', summary: 'Failed', detail: 'Tracker creation failed', life: 3000 });
+            }
+            setTrackerDialog(false)
+
+        })
+    }
+
+    const trackerDialogFooter = (
+        <>
+            <Button label="Cancel" icon="pi pi-times" text onClick={hideDialog} />
+            <Button label="Save" icon="pi pi-check" text onClick={saveProduct} />
+        </>
+    )
 
     return (
         <div className="grid crud-demo">
@@ -75,11 +152,34 @@ export default function Tracker() {
                         responsiveLayout="scroll">
 
                         <Column selectionMode="multiple" headerStyle={{ width: '4rem' }}></Column>
+                        <Column field="campaignName" header="Campaign" sortable headerStyle={{ minWidth: '15rem' }}></Column>
                         <Column field="createdBy" header="Creator" sortable headerStyle={{ minWidth: '15rem' }}></Column>
                         <Column field="restricted" header="Private" sortable headerStyle={{ minWidth: '15rem' }}></Column>                        
                         <Column field="vendor" header="Vendor" sortable></Column>
                         <Column field="creationTime" header="Created at" sortable headerStyle={{ minWidth: '10rem' }}></Column>
-                    </DataTable>                     
+                    </DataTable>
+                    <Dialog visible={trackerDialog} header="New Campaign to Track" modal className="p-fluid" onHide={hideDialog} footer={trackerDialogFooter}>
+                        <div className="field">
+                                <label htmlFor="campaignName">Campaign</label>
+                                <InputText id="campaignName" value={tracker.campaignName} onChange={(e) => onInputChange(e, 'campaignName')} required autoFocus className={classNames({ 'p-invalid': submitted && !tracker.campaignName })} />
+                                {submitted && !tracker.campaignName && <small className="p-invalid">Campaign name is required.</small>}
+                        </div>
+                        <div className="field">
+                                <label htmlFor="createdBy">Created By</label>
+                                <InputText id="createdBy" value={tracker.createdBy} onChange={(e) => onInputChange(e, 'createdBy')} required disabled className={classNames({ 'p-invalid': submitted && !tracker.createdBy })} />
+                                {submitted && !tracker.createdBy && <small className="p-invalid">Creator is required.</small>}
+                        </div> 
+                        <div className="field">
+                                <label htmlFor="restricted">Restricted</label>
+                                <br />
+                                <Checkbox id="restricted" value={tracker.restricted} onChange={(e) => onCheckboxChanged(e, 'restricted')} checked={tracker.restricted} autoFocus />
+                        </div>
+                        <div className="field">
+                                <label htmlFor="vendor">Vendor</label>
+                                <Dropdown value={tracker.vendor} options={vendors} onChange={(e) => onDropdownChange(e, 'vendor')} optionLabel="name" placeholder="Select a Vendor" className={classNames({ 'p-invalid': submitted && !tracker.vendor })} />
+                                {submitted && !tracker.vendor && <small className="p-invalid">Creator is required.</small>}
+                        </div>                         
+                    </Dialog>
                 </div>
             </div>
         </div>
--- src/service/TrackerService.js
+++ src/service/TrackerService.js
@@ -1,6 +1,13 @@
+import axios from 'axios';
+
 export const TrackerService = {
     getTrackerMetaData() {
-        return fetch('http://localhost:8080/campaigns', { headers: { 'Cache-Control': 'no-cache' } })
-            .then((res) => res.json());
+        return axios.get('http://localhost:8080/campaigns')
+            .then((res) => res.data);
+    },
+
+    saveTrackerMeta(tracker, callBack) {
+        return axios.post('http://localhost:8080/campaign', tracker)
+            .then((response) => callBack(response.data))
     }
 }
